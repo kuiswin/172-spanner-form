71fdf6534ec3cb89685116e3eb86fb853ed90316
kuiswin <kuiswin@users.noreply.github.com>
2026-07-19 10:49:35 +0900

    chore: auto-sync sandbox code

---
+++ schema.sql
@@ -0,0 +1,16 @@
+-- 親テーブル
+CREATE TABLE orders (
+  id VARCHAR(36) PRIMARY KEY,
+  customer_name VARCHAR(100),
+  delivery_address VARCHAR(255),
+  created_at TIMESTAMPTZ
+);
+
+-- 子テーブル
+CREATE TABLE order_items (
+  id VARCHAR(36),
+  item_id VARCHAR(36),
+  item_name VARCHAR(100),
+  quantity BIGINT,
+  PRIMARY KEY (id, item_id)
+) INTERLEAVE IN PARENT orders ON DELETE CASCADE;
