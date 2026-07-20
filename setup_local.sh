26ca3faef1029893954bff2e1ff5f4b758f54318
root <git@kuis.win>
2026-07-20 13:27:47 +0900

    chore: auto-sync sandbox code

--- setup_local.sh
+++ setup_local.sh
@@ -1,5 +1,5 @@
 # 依存ツールのインストール (未導入の場合)
-sudo apt-get update && sudo apt-get install -y docker.io docker-compose-v2 git
+sudo apt-get update && sudo apt-get install -y docker.io docker-compose-v2 git postgresql-client
 
 # リポジトリのクローンと作業ディレクトリへの移動
 git clone https://github.com/kuiswin/172-spanner-form.git sandbox_172
