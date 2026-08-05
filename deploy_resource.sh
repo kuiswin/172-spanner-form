9fac50f468fd28bdf0ed1051a0c36cd226ab7aa9
kuiswin <kuiswin@users.noreply.github.com>
2026-08-05 22:22:53 +0900

    chore: auto-sync sandbox code

--- deploy_resource.sh
+++ deploy_resource.sh
@@ -6,14 +6,14 @@ gcloud run deploy ${SERVICE_NAME} \
     --allow-unauthenticated \
     --service-account=${SA_EMAIL} \
     --execution-environment=gen1 \
-    --volume=name=sockets-dir,type=in-memory,size=50Mi \
+    --add-volume=name=sockets-dir,type=in-memory,size=50Mi \
     --container pgadapter \
       --image="gcr.io/cloud-spanner-pg-adapter/pgadapter:latest" \
       --args="-p,${PROJECT_ID},-i,main-spanner-instance,-dir,/sockets" \
-      --volume-mount=name=sockets-dir,mount-path=/sockets \
+      --add-volume-mount=volume=sockets-dir,mount-path=/sockets \
     --container app \
       --depends-on=pgadapter \
       --set-env-vars="DATABASE_URL=host=/sockets port=5432 user=postgres dbname=delivery-db sslmode=disable" \
       --port 80 \
-      --volume-mount=name=sockets-dir,mount-path=/sockets \
+      --add-volume-mount=volume=sockets-dir,mount-path=/sockets \
     --max-instances 10
